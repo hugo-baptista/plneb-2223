692e118f11b67581d81957a13566216e370cbd5f
hugo-baptista <pg50416@alunos.uminho.pt>
2023-03-09 11:42:01 +0000

    Aula 3

---
+++ trabalhos_aulas/aula_3/text_to_html.py
@@ -0,0 +1,35 @@
+import re
+
+file = open("plneb-2223/data/dicionario_medico.txt", "r", encoding="utf-8")
+text = file.read()
+
+remove_form_feed = re.sub(r"\f", "", text)
+mark_terms = re.sub(r"\n\n(.+)", r"\n\n#T=\1", remove_form_feed)
+mark_explications = re.sub(r"\n([^#\s].+)", r"\n#E=\1", mark_terms)
+correct_expl = re.sub(r"(#T=.+)\n\n#T=(.+)", r"\1\n#E=\2", mark_explications)
+remove_new_lines = re.sub(r"(#E=.+)(?:\n#E=(.+))+", r"\1 \2", correct_expl)
+remove_marks = re.sub(r"#[TE]=", r"", remove_new_lines)
+entries = re.findall(r"\n\n(.+)\n(.+)", remove_marks)
+
+html = open("trabalhos_aulas/aula_3/dicionario_medico.html", "w")
+
+header = """
+<html>
+<head>
+<meta charset='uft-8' />
+</head>
+<body>
+"""
+
+body = ""
+for entry in entries:
+    body += "<b>" + entry[0] + "</b>"
+    body += "<p>" + entry[1] + "</p>"
+    body += "<hr>"
+
+footer = """
+</body>
+</html>
+"""
+
+html.write(header+body+footer)
