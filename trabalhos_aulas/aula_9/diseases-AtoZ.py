0889fc77417f54a046e206e8e984cdf30a2665b7
hugo-baptista <pg50416@alunos.uminho.pt>
2023-05-25 10:14:06 +0100

    Update Nomes Aula9

---
+++ trabalhos_aulas/aula_9/diseases-AtoZ.py
@@ -0,0 +1,31 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+disease_dic = {}
+
+base_url = 'https://www.atlasdasaude.pt'
+main_url = '/doencasAaZ'
+main_html = requests.get(base_url+main_url).text
+
+main_soup = BeautifulSoup(main_html, 'html.parser')
+
+divs = main_soup.find_all('div', class_='views-summary views-summary-unformatted')
+
+for div in divs:
+    page_url = div.a.get('href')
+
+    html = requests.get(base_url+page_url).text
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    diseases = soup.find_all('div', class_='views-row')
+
+    for disease in diseases:
+        title = disease.find('div', class_='views-field-title').text
+        designation = disease.find('div', class_='views-field-body').text
+        # print(f'Title: {title}\nDesignation: {designation}\n')
+        disease_dic[title] = designation
+
+file = open('./trabalhos_aulas/aula_9/db-diseases-AtoZ.json', 'w', encoding='utf-8')
+json.dump(disease_dic, file, indent=4, ensure_ascii=False)
+file.close()
